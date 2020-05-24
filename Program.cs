42979bc5862d4be8f09fb848522057457a4728eb
tenziin <thendrup@gmail.com>
2020-05-24 20:15:08 +1000

    first commit

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Assessment1a
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new main());
         }
     }
 }
